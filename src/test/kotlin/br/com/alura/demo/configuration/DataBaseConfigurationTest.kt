0dd85f77790310ac8dea82f81229943ab4bd49d8
Jhon Lynikeer <jhonlynikeer@gmail.com>
2024-11-20 02:01:13 -0300

    reconfigurar-CI

--- src/test/kotlin/br/com/alura/demo/configuration/DataBaseConfigurationTest.kt
+++ src/test/kotlin/br/com/alura/demo/configuration/DataBaseConfigurationTest.kt
@@ -5,6 +5,7 @@ import br.com.alura.demo.repository.TopicoRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.GenericContainer
@@ -13,6 +14,7 @@ import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class DataBaseConfigurationTest {
 
     companion object {
@@ -21,23 +23,26 @@ class DataBaseConfigurationTest {
             withDatabaseName("testdb")
             withUsername("joao")
             withPassword("12345")
+            start() // Garante que o MySQL container seja iniciado
         }
 
-
-        private val redisContainer = GenericContainer<Nothing>("redis:alpine3.20").apply {
+        @Container
+        private val redisContainer = GenericContainer<Nothing>("redis:latest").apply {
             withExposedPorts(6379)
-
+            start() // Garante que o Redis container seja iniciado
         }
 
         @JvmStatic
         @DynamicPropertySource
         fun properties(registry: DynamicPropertyRegistry) {
-            registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
-            registry.add("spring.datasource.password", mysqlContainer::getPassword);
-            registry.add("spring.datasource.username", mysqlContainer::getUsername);
+            // Configurações do MySQL
+            registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl)
+            registry.add("spring.datasource.password", mysqlContainer::getPassword)
+            registry.add("spring.datasource.username", mysqlContainer::getUsername)
 
-            registry.add("spring.redis.host", redisContainer::getContainerIpAddress)
-            registry.add("spring.redis.host", redisContainer::getFirstMappedPort)
+            // Configurações do Redis
+            registry.add("spring.redis.host", redisContainer::getHost)
+            registry.add("spring.redis.port", redisContainer::getFirstMappedPort)
         }
     }
 }
